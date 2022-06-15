f5ce0e0f09d2778de48f973c33c944936d809d45
Dubesor22 <dubesor22@gmail.com>
2022-06-15 14:57:17 +0200

    contact-responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar sticky-top">
+      <nav class=" navbar sticky-top">
         <div class="container-nav">
           <a class="navbar-brand" href="#main">
             David Ubeda
